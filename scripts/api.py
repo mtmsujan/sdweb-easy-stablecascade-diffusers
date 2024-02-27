7ea88013058a54b8a494b6504ead1eb5d38f3388
Md Toriqul Mowla <sujan@Mds-MacBook-Pro.local>
2024-02-27 17:42:26 +0600

    Added API for text to image

---
+++ scripts/api.py
@@ -0,0 +1,125 @@
+from fastapi import FastAPI, Body
+
+from modules.api.models import *
+from modules.api import api
+import gradio as gr
+import torch
+import gc
+import json
+from diffusers import StableCascadeDecoderPipeline, StableCascadePriorPipeline
+from modules import script_callbacks, images
+from modules.processing import get_fixed_seed
+from modules.rng import create_generator
+from modules.shared import opts
+from modules.ui_components import ResizeHandleRow
+
+# modules/infotext_utils.py
+def quote(text):
+    if ',' not in str(text) and '\n' not in str(text) and ':' not in str(text):
+        return text
+
+    return json.dumps(text, ensure_ascii=False)
+
+# modules/processing.py
+def create_infotext(prompt, negative_prompt, guidence_scale, prior_steps, decoder_steps, seed, width, height):
+    generation_params = {
+        "Model": "StableCascade",
+        "Size": f"{width}x{height}",
+        "Seed": seed,
+        "Steps(Prior)": prior_steps,
+        "Steps(Decoder)": decoder_steps,
+        "CFG": guidence_scale,
+        "RNG": opts.randn_source if opts.randn_source != "GPU" else None
+    }
+
+    generation_params_text = ", ".join([k if k == v else f'{k}: {quote(v)}' for k, v in generation_params.items() if v is not None])
+
+    prompt_text = prompt
+    negative_prompt_text = f"\nNegative prompt: {negative_prompt}" if negative_prompt else ""
+
+    return f"{prompt_text}{negative_prompt_text}\n{generation_params_text}".strip()
+
+def predict(prompt, negative_prompt, width, height, guidance_scale, prior_steps, decoder_steps, seed, batch_size):
+    device = "cuda"
+    prior = StableCascadePriorPipeline.from_pretrained("stabilityai/stable-cascade-prior", torch_dtype=torch.bfloat16).to(device)
+
+    fixed_seed = get_fixed_seed(seed)
+    prior_output = prior(
+        prompt=prompt,
+        negative_prompt=negative_prompt,
+        width=width,
+        height=height,
+        guidance_scale=guidance_scale,
+        num_inference_steps=prior_steps,
+        num_images_per_prompt=batch_size,
+        generator=create_generator(fixed_seed)
+    )
+    del prior
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    decoder = StableCascadeDecoderPipeline.from_pretrained("stabilityai/stable-cascade",  torch_dtype=torch.float16).to(device)
+    decoder_output = decoder(
+        image_embeddings=prior_output.image_embeddings.half(),
+        prompt=prompt,
+        negative_prompt=negative_prompt,
+        guidance_scale=0.0,
+        output_type="pil",
+        num_inference_steps=decoder_steps
+    ).images
+    del decoder
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    for image in decoder_output:
+        images.save_image(
+            image,
+            opts.outdir_samples or opts.outdir_txt2img_samples,
+            "",
+            fixed_seed,
+            prompt,
+            opts.samples_format,
+            info=create_infotext(prompt, negative_prompt, guidance_scale, prior_steps, decoder_steps, fixed_seed, width, height)
+        )
+
+    return decoder_output
+
+def cascade_api(_: gr.Blocks, app: FastAPI):
+    @app.post("/cascade")
+    async def stable_cascade(
+        prompt: str = Body(...),
+        negative_prompt: str = Body(""),
+        width: int = Body(1024),
+        height: int = Body(1024),
+        guidance_scale: float = Body(4.0),
+        prior_steps: int = Body(20),
+        decoder_steps: int = Body(10),
+        seed: int = Body(-1),
+        batch_size: int = Body(1)
+    ):
+        image = predict(prompt, negative_prompt, width, height, guidance_scale, prior_steps, decoder_steps, seed, batch_size)[0]
+        
+        return {
+            "images": [
+                api.encode_pil_to_base64(image).decode("utf-8")
+            ],
+            "parameters" : {
+                "prompt": prompt,
+                "negative_prompt": negative_prompt,
+                "width": width,
+                "height": height,
+                "guidance_scale": guidance_scale,
+                "prior_steps": prior_steps,
+                "decoder_steps": decoder_steps,
+                "seed": seed,
+                "batch_size": batch_size
+            },
+            "info": create_infotext(prompt, negative_prompt, guidance_scale, prior_steps, decoder_steps, seed, width, height)
+        }
+
+try:
+    import modules.script_callbacks as script_callbacks
+
+    script_callbacks.on_app_started(cascade_api)
+except:
+    pass
