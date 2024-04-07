d7ad49edffbcba72d2abdadca909fd35647d6716
mtmsujon <mtmsujon@gmail.com>
2024-04-08 00:30:01 +0600

    upgrade support for diffusers on installation

--- install.py
+++ install.py
@@ -3,4 +3,4 @@ import launch
 try:
   from diffusers import StableCascadeDecoderPipeline
 except:
-  launch.run_pip(f"install git+https://github.com/kashif/diffusers.git@a3dc21385b7386beb3dab3a9845962ede6765887", "diffusers@wuerstchen-v3")
+  launch.run_pip(f"install --upgrade diffusers")
